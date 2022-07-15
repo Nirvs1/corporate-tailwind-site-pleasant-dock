a4fc666d9bdd6058d12fd4680a8fce4a42eb61b6
Nirvan <93690524+Nirvs1@users.noreply.github.com>
2022-07-15 20:44:58 +0100

    First commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Block2 from "@/templates/blocks/block-2/block-2";
+import List2 from "@/templates/lists/list-2/list-2";
+import Cards2 from "@/templates/cards/cards-2/cards-2";
+import Cover5 from "@/templates/covers/cover-5/cover-5";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.homeSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover5 content={props.mainLandingContent} />
+			 <Cards2 content={props.featuresListContent} />
+			 <List2 content={props.benefitsListContent} />
+			 <Block2 content={props.aboutTeaserContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'home'});
+  return {
+    props: props
+  };
+}
---
+++ pages/services.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Banner4 from "@/templates/banners/banner-4/banner-4";
+import Block2 from "@/templates/blocks/block-2/block-2";
+import Cover2 from "@/templates/covers/cover-2/cover-2";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.servicesSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover2 content={props.servicesLandingContent} />
+			 <Block2 content={props.servicesBlockContent} />
+			 <Banner4 content={props.contactTeaserContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'service'});
+  return {
+    props: props
+  };
+}
